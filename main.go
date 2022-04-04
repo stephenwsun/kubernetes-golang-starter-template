87a1edd845254640804905de7ee415e694e25deb
Stephen Sun <stephensun.me@gmail.com>
2022-04-04 14:48:56 -0500

    Add example to stream pod logs

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
+	corev1 "k8s.io/api/core/v1"
+	"log"
 	"path/filepath"
 	"time"
 
@@ -45,17 +48,49 @@ func main() {
 		// - Use helper functions like e.g. errors.IsNotFound()
 		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
 		namespace := "default"
-		pod := "example-xxxxx"
-		_, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
+		podName := "test-pod-name" // Replace with your pod name
+		_, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), podName, metav1.GetOptions{})
 		if errors.IsNotFound(err) {
-			fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
+			fmt.Printf("Pod %s in namespace %s not found\n", podName, namespace)
 		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
 			fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-				pod, namespace, statusError.ErrStatus.Message)
+				podName, namespace, statusError.ErrStatus.Message)
 		} else if err != nil {
 			panic(err.Error())
 		} else {
-			fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
+			fmt.Printf("Found pod %s in namespace %s\n", podName, namespace)
+		}
+
+		// Get logs
+		logOptions := &corev1.PodLogOptions{
+			Follow:     true,
+			Timestamps: true,
+		}
+
+		podLogs := clientset.CoreV1().Pods(namespace).GetLogs(podName, logOptions)
+
+		stream, err := podLogs.Stream(context.Background())
+		if err != nil {
+			log.Println([]byte(err.Error()))
+			return
+		}
+		defer stream.Close()
+
+		for {
+			buf := make([]byte, 2000)
+			numBytes, err := stream.Read(buf)
+			if numBytes == 0 {
+				continue
+			}
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Println([]byte(err.Error()))
+				break
+			}
+
+			fmt.Println("--------NEW_LOGS", string(buf[:numBytes]))
 		}
 
 		time.Sleep(10 * time.Second)
